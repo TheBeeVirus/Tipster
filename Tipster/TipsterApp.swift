48d6497293a3ffd66c17c549bff899c399d7f9d1
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-08-20 18:46:21 -0700

    Initial Commit

---
+++ Tipster/TipsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsterApp.swift
+//  Tipster
+//
+//  Created by Jd Beecham on 8/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TipsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
