f48e33b7c6374025b210b69a7303f12f5a7ca531
Jd Beecham <TheBeeVirus@users.noreply.github.com>
2021-08-25 16:17:13 -0700

    Completed

---
+++ Tipster/CalculateView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculateView.swift
+//  Tipster
+//
+//  Created by Jd Beecham on 8/23/21.
+//
+
+import SwiftUI
+
+struct CalculateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculateView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculateView()
+    }
+}
